60a8437b7a5916f5f982e576a94fac9c7ebff46e
Yvo van Zee <yvanzee@anwb.nl>
2025-01-08 08:34:07 +0100

    init

--- lib/service-catalog-stack.ts
+++ lib/service-catalog-stack.ts
@@ -2,15 +2,44 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+class CDKBootstrapProduct extends cdk.aws_servicecatalog.ProductStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const bucketKey = new cdk.aws_kms.Key(this, 'FileAssetsBucketKey', {
+      enableKeyRotation: true,
+      alias: 'cdk-assets-key'
+    })
+    
+    new cdk.aws_s3.Bucket(this, 'StagingBucket', {
+      encryption: bucketKey,
+      
+    })
+  }
+}
 export class ServiceCatalogStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const portfolio = new cdk.aws_servicecatalog.Portfolio(this, 'Portfolio', {
+      displayName: 'CCoE-PortFolio',
+      providerName: 'CCOE',
+      description: 'Portfolio with list of Applications created by Cloud Center of Enablement',	
+      messageLanguage: cdk.aws_servicecatalog.MessageLanguage.EN
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServiceCatalogQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cdkBootstrap = new cdk.aws_servicecatalog.CloudFormationProduct(this, 'CDKBootstrap', {
+      productName: 'CDKBootstrap',
+      owner: 'CCOE',
+      productVersions: [
+        {
+          productVersionName: 'v1.0',
+          cloudFormationTemplate: cdk.aws_servicecatalog.CloudFormationTemplate.fromProductStack(new cdk.aws_servicecatalog.ProductStack(this, 'CDKBootstrapStack')),
+          description: 'CDKBootstrap is a product that deploys CDKBootstrap'
+        }
+      ],
+      description: 'CDKBootstrap is a product that deploys CDKBootstrap',
+      distributor: 'CCOE',
+    });
   }
 }
